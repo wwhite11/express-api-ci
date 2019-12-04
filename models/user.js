8241f1bf5e5b5f91be1cb4e079151840fd3f6e06
Winston94 <2winstonrwhite@gmail.com>
2019-12-04 17:03:44 -0500

    about to push

---
+++ models/user.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+    User.hasMany(models.Project, {
+      foreignKey: 'userId'
+    })
+  };
+  return User;
+};
